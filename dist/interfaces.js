670e022056c2e1a9b4abe75d064767c01dc4522c
mourad barkouch <mouradbarkouch@gmail.com>
2024-09-29 00:33:56 +0100

    interfaces

---
+++ dist/interfaces.js
@@ -0,0 +1,40 @@
+"use strict";
+let user = {
+    id: 101,
+    userName: 'M.BARKOUCH',
+    country: 'MAR',
+    toString() {
+        return `
+        Id : ${this.id}
+        User name : ${this.userName}
+        Country : ${this.country}
+        `;
+    }
+};
+console.log(user.toString());
+console.log('·'.repeat(100));
+console.log('\n');
+let theme = {
+    dark: true,
+    font: 'Poppins',
+    fontSize: 16
+};
+console.log('·'.repeat(100));
+console.log('\n');
+let member_ = {
+    id: 102,
+    font: 'Poppins',
+    fontSize: 16,
+    userName: 'M.BARKOUCH',
+    country: 'MAR',
+    email: 'mouradbarkouch@gmail.com',
+    toString() {
+        return `
+        Id : ${this.id}
+        User name : ${this.userName}
+        Country : ${this.country}
+        E-mail : ${this.email}
+        `;
+    }
+};
+console.log(member_.toString());
---
+++ src/interfaces.ts
@@ -0,0 +1,74 @@
+
+
+interface User{
+    id : number,
+    userName : string,
+    country : string,
+    toString() : string
+}
+
+let user : User = {
+    id : 101,
+    userName : 'M.BARKOUCH',
+    country : 'MAR',
+    toString() : string {
+        return `
+        Id : ${this.id}
+        User name : ${this.userName}
+        Country : ${this.country}
+        `
+    }
+
+}
+
+console.log(user.toString())
+
+console.log('·'.repeat(100));
+console.log('\n')
+
+// re-open the interface 
+
+interface Theme{
+    readonly dark?: boolean,
+    font: string,
+}
+
+interface Theme{
+    fontSize: number,
+}
+
+let theme : Theme = {
+    dark : true,
+    font : 'Poppins',
+    fontSize: 16
+}
+
+
+console.log('·'.repeat(100));
+console.log('\n')
+
+// interface extends
+
+interface Member extends User, Theme{
+    email : string
+}
+
+
+let member_ : Member = {
+    id : 102,
+    font : 'Poppins',
+    fontSize: 16,
+    userName : 'M.BARKOUCH',
+    country : 'MAR',
+    email : 'mouradbarkouch@gmail.com',
+    toString() : string {
+        return `
+        Id : ${this.id}
+        User name : ${this.userName}
+        Country : ${this.country}
+        E-mail : ${this.email}
+        `
+    }
+}
+
+console.log(member_.toString())
