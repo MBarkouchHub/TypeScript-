8ae6cff0a604c8e1d1bca8c4a1d19ac638e768a8
mourad barkouch <mouradbarkouch@gmail.com>
2024-09-28 21:48:08 +0100

    Tuple

---
+++ dist/Tuple.js
@@ -0,0 +1,7 @@
+"use strict";
+let person = [11, 'Mourad', 27];
+console.log(person);
+const [id, nom, age_] = person;
+console.log(id);
+console.log(nom);
+console.log(age_);
---
+++ src/Tuple.ts
@@ -0,0 +1,16 @@
+
+
+
+
+let person : readonly [number, string, number]  = [11, 'Mourad', 27];
+ 
+// person.push(100);
+
+console.log(person);
+
+
+const [id, nom, age_]  = person;
+
+console.log(id);
+console.log(nom);
+console.log(age_);
